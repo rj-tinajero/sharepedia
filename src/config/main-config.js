7e0d9cc909aef5e8dc7a3eae5625005c10ba8394
Richard Tinajero <rj.tinajero92@gmail.com>
2019-04-22 16:31:45 -0700

    WIP deploy to production

---
+++ src/config/main-config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const logger = require('morgan');
+
+module.exports = {
+  init(app, express){
+      app.set("views", viewsFolder);
+      app.set("view engine", "ejs");
+      app.use(bodyParser.urlencoded({extended: true}));
+      app.use(logger('dev'));
+  }
+};
