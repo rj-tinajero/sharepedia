1841e9703b3f2fb5e2a73448a6edf71790ae1d42
Richard Tinajero <rj.tinajero92@gmail.com>
2019-04-27 10:35:45 -0700

    CRUD done

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,6 +1,5 @@
 module.exports = {
     index(req, res, next) {
-        console.log(res);
         res.render("static/index", {title: "Welcome to Sharepedia"});
     }
 }
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -15,6 +15,10 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Wiki, {
+      foreignKey: "userId",
+      as: "wikis"
+    });
   };
   
   return User;
